b979eb129b2a6a78436ae67d8a4acb401f9913c1
kalush81 <krzysiek.kaluza@gmail.com>
2021-04-06 14:26:56 +0200

    ready to netlify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import { useState } from 'react'
+import * as Survey from "survey-react";
+import elements from "./elements";
+import "survey-react/modern.css";
+import { Route, Switch } from "react-router-dom";
+import Home from "./home";
+import SurveyCompo from './survey'
+import React from "react";
+
+Survey.StylesManager.applyTheme("modern");
 
 function App() {
+  const [isRead , setRead] = useState(false)
+  console.log(elements);
+  let json = {
+    pages: [{ questions: elements }],
+  };
+
+  const model = new Survey.Model(json);
+  model.completedHtml = "dziekujemy za wypelnienie ankiety";
+  model.showQuestionNumbers = "on";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path="/survey">
+          <SurveyCompo isRead={isRead}>
+            <Survey.Survey
+              model={model}
+              onComplete={(result) => {
+                console.log("json result: ...", result.data);
+                console.log("res.data type", typeof result.data);
+              }}
+            />
+          </SurveyCompo>
+        </Route>
+        <Route path="/">
+          <Home setIsRead={setRead}/>
+        </Route>
+      </Switch>
     </div>
   );
 }
